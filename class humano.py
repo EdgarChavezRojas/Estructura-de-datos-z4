b23b612b2b9298ce4ae33cc3a90f64121fed7ada
EdgarChavezRojas <159058871+EdgarChavezRojas@users.noreply.github.com>
2024-03-05 19:11:39 -0400

    Add files via upload

---
+++ class humano.py
@@ -0,0 +1,7 @@
+class Humano():
+    def __init__(self, edad, nombre):
+         self .edad = edad
+         self.nombre = nombre
+        
+Persona1 = Humano(22, "Pedro")
+Persona2 = Humano(41, "Edwin")
